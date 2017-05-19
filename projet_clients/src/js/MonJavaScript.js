7e024e09dd594e6de03e42e214a6bdd891dc62dd
Yoan Weibel <yoan.weibel@rpn.ch>
2017-05-19 09:07:01 +0200

    end of the layout and start the js

---
+++ projet_clients/src/js/MonJavaScript.js
@@ -0,0 +1,9 @@
+var tr1 = document.createElement("tr");
+var td1 = document.createElement("td");
+var td2 = document.createElement("td");
+var t1 = document.createTextNode("");
+var t2 = document.createTextNode("");
+function FonctionBoutonAjout(){
+	t1 = document.createTextNode(documentt.getElementById(NomAjout).textContent);
+	t2 = document.createTextNode(documentt.getElementById(PrenomAjout).textContent);
+}
